f1462ab7fd370426390f56c890343680519b0d78
Ubuntu <ubuntu@ip-172-31-35-94.ec2.internal>
2020-11-19 19:19:06 +0000

    initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Douglass Jeffrey
+ * Created on November 18
+ * Runs the Hello World! program
+ * Class HelloWorld.
+ */
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
